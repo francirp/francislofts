89e79f4e1ab16611d06272e4438aee743ff76470
Ryan Francis <ryan.p.francis@gmail.com>
2014-06-08 21:38:37 -0500

    fixing precompile issue

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Francislofts
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
